b321419a622e352e2bc5af40f3349fb76ce9e4f3
AkikoYamamotoBeraud <akikoyamamotoberaud@gmail.com>
2024-03-25 08:59:38 +0100

    admin button

--- functions.php
+++ functions.php
@@ -6,9 +6,7 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/theme.css', array(), filemtime(get_stylesheet_directory() . '/theme.css'));
 }
-?>
 
-<?php
 
 add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
 
@@ -17,6 +15,17 @@ function add_admin_link($items, $args) {
         $menu_items = explode('</li>', $items);
 
         $admin_link = '<li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42"><a href="'. get_admin_url() .'" class="menu-link">Admin</a></li>';
+                      
+        array_splice($menu_items, 1, 0, $admin_link);
+
+        $items = implode('</li>', $menu_items);
+        $items .= '</li>'; 
+    }
+    if (is_user_logged_in() && $args->theme_location == 'mobile_menu') {
+        $menu_items = explode('</li>', $items);
+
+        $admin_link = '<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-36 current_page_item menu-item-45"><a href="'. get_admin_url() .'" class="menu-link">Admin</a></li>';
+                      
         array_splice($menu_items, 1, 0, $admin_link);
 
         $items = implode('</li>', $menu_items);
@@ -26,4 +35,3 @@ function add_admin_link($items, $args) {
 }
 
 ?>
-
